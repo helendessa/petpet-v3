ea675a5b24528acd6af94f850ed8db2b628c5648
helendessa <helendessa@gmail.com>
2025-01-15 15:39:54 -0300

    initial commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "Signup route works!",
+    });
+}
+
+export const login = async (req, res) => {
+    res.json({
+        data: "Login route works!",
+    });
+}
+
+export const logout = async (req, res) => {
+    res.json({
+        data: "Logout route works!",
+    });
+}
