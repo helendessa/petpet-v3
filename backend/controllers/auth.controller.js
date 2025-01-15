bdc40216d0bed1173ec0fa51c2d13322451a67e8
helendessa <helendessa@gmail.com>
2025-01-15 18:37:15 -0300

    fix routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -25,6 +25,9 @@ export const signup = async (req, res) => {
         }
 
         // check password
+        if(password.length < 6) {
+            return res.status(400).json({ error: "Password must be at least 6 characters long" });
+        }        
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -77,13 +80,39 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.json({
-        data: "Login route works!",
-    });
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            username: user.username,
+            fullName: user.fullName,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImage: user.profileImage,
+            coverImg: user.coverImg,
+        });
+    } catch (error) {
+        console.log("Error at login controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 export const logout = async (req, res) => {
-    res.json({
-        data: "Logout route works!",
-    });
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error at logout controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
