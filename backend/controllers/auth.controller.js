d82d37b5e7d5e0b13d4f56c4d9db5f3d13c74a29
helendessa <helendessa@gmail.com>
2025-01-15 19:17:06 -0300

    add get me route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -116,3 +116,14 @@ export const logout = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 }
+
+export const getMe = async (req, res) => { 
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+        
+    } catch (error) {
+        console.log("Error at getMe controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongoDB.js';
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000 || 7000;
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data
 
-//console.log(process.env.MONGO_URI);
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
