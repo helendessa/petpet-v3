cb1611bf0590d3193b85d03df3d531f3823c72eb
helendessa <helendessa@gmail.com>
2025-01-16 22:56:35 -0300

    add post

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,34 @@
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+import { v2 as cloudinary } from 'cloudinary';
+
+export const createPost = async (req, res) => {
+    try {
+        const { text } = req.body;
+        let { img } = req.body;
+        const userId = req.user._id.toString();
+
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({ error: "User not found" })
+        
+        if(!text && !img) return res.status(400).json({ error: "Text or Image is required" })
+        
+        if(img) {
+            const uploadedResponse = await cloudinary.uploader.upload(img);
+            img = uploadedResponse.secure_url;
+        }
+
+        const newPost = new Post({
+            user: userId,
+            text,
+            img,
+        });
+
+        await newPost.save();
+        res.status(201).json(newPost);
+        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log("Error in createPost: ", error.message);
+    }
+}
