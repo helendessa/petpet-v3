5843d5b5f127ab165bd1d35c5684f7e20fe34b4a
helendessa <helendessa@gmail.com>
2025-01-18 12:37:49 -0300

    add skeletons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,24 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
+import NotificationPage from './components/notification/NotificationPage.jsx';
+import ProfilePage from './pages/profile/ProfilePage';
+
+import Sidebar from './components/common/Sidebar.jsx';
+import RightPanel from './components/common/RightPanel.jsx';
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
---
+++ frontend/src/components/svgs/X.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const X = () => {
+  return (
+    <img
+      src="https://internetlab.org.br/wp-content/uploads/2016/05/quadrado-vermelho.png"
+      alt="Red Square"
+      className="w-6 h-6"
+    />
+  );
+};
+
+export default X;
